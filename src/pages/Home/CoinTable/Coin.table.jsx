24a87d2ed5202a518bb9573835c13e0dc6a451c1
Ashish-simpleCoder <ashishprajapati357753357@gmail.com>
2022-01-20 10:43:03 +0530

    removing styles of table from Coin table

--- src/pages/Home/CoinTable/Coin.table.jsx
+++ src/pages/Home/CoinTable/Coin.table.jsx
@@ -1,12 +1,10 @@
 import { memo} from "react"
-import styled from "styled-components"
-import TableHead from "./Table.head"
-import TableBody from "./Table.body"
 import Table from "../../../components/table/Table"
 import CryptoState from "../../../context/CryptoCtx"
 
 const table_heads = ['Coin','Price','24h Change','Market Cap']
 const styles = {borderCollapse:'collapse',minWidth:'60rem'}
+
 
 const CoinTable = memo(()=>{
     const {TABLE_COINS, start, end} = CryptoState()
@@ -15,26 +13,4 @@ const CoinTable = memo(()=>{
     )
 })
 export default CoinTable
-
-
-const StyledTable = styled.table`
-    text-align:center;
-    width:100%;
-    background:var(--table-bg);
-    color:var(--prm-clr);
-    max-width:90rem;
-    margin:auto;
-    tbody,tr,td{
-        border:1px solid var(--border);
-        border-collapse:collapse;
-    }
-    td{
-        padding:1rem;
-        img{
-            width:3rem;
-            height:3rem;
-            margin-inline-end:2rem;
-        }
-    }
-`
 
--- src/pages/Home/CoinTable/CoinTableContainer.jsx
+++ src/pages/Home/CoinTable/CoinTableContainer.jsx
@@ -15,7 +15,7 @@ const CoinTableContainer = memo(({heading, visible})=>{
         <section style={{padding:'3rem clamp(1rem,3rem,3vw)',textAlign:'center',height:'100%'}}>
             <h2 style={{fontSize:'clamp(2rem,2.6rem,2.6vw)',margin:'1rem 0 2rem 0'}}>{heading}</h2>
             <section style={{width:'100%',overflowX:'auto'}}>
-            <CoinTable/>
+                <CoinTable/>
             </section>
             <Pagination/>
         </section>
