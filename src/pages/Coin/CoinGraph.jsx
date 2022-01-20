c83fb65254ed0618ebd3b1b6da490a71c8ea7348
Ashish-simpleCoder <ashishprajapati357753357@gmail.com>
2022-01-20 11:07:56 +0530

    added heading of graph component

--- src/pages/Coin/CoinGraph.jsx
+++ src/pages/Coin/CoinGraph.jsx
@@ -1,16 +1,20 @@
 import { memo, useLayoutEffect } from "react";
 import styled from "styled-components";
 import Graph from "../../components/Graph";
+import H2 from "../../components/H2";
 import CryptoState from "../../context/CryptoCtx";
 import { fetchCoinHistory } from "../../utils/fetchCoinsDetails";
+
 
 const CoinGraph = memo(({id, visible})=>{
     const {history, currency, setCoin} = CryptoState()
     useLayoutEffect(()=>{
         visible && fetchCoinHistory(id, history.days, currency).then(({prices})=>setCoin(v=>({...v,history:{...v.history,data:prices}})))
     },[currency,id,setCoin,visible,history.days])
+
     return(
         <StyledGraphContainer id='coin_graph'>
+            <H2 text={`Coin history upto ${history.days} days`} center mb/>
             <Graph data={history.data} days={history.days} currency={currency}/>
         </StyledGraphContainer>
     )
